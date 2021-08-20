092008c529a9ea5e3319158b8d6c112d61066bfb
Buğra Göksu <bugragoksu2@gmail.com>
2021-08-21 00:39:11 +0300

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:virtual_study_app/src/screens/auth/auth_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MaterialApp(debugShowCheckedModeBanner: false, home: AuthScreen()));
 }
